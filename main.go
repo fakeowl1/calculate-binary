00679419d60d185a172e27cf7af3843432ea6f2e
fakeowl <99616768+fakeowl1@users.noreply.github.com>
2026-03-05 14:08:46 +0000

    Update main.go

--- main.go
+++ main.go
@@ -18,25 +18,25 @@ func main() {
 
 	// --- BYTE (8 bit) ---
 	bitsA8 := ConvertNumberToBinaryArray(uint(A_val), 8)
-	fmt.Printf("Byte A (28):   %v\n", bitsA8)
-	fmt.Printf("Byte -A (-28): %v\n", GetTwosComplement(bitsA8, 8))
+	fmt.Printf("Byte A (13):   %v\n", bitsA8)
+	fmt.Printf("Byte -A (-13): %v\n", GetTwosComplement(bitsA8, 8))
 
 	// --- WORD (16 bit) ---
 	bitsA16 := ConvertNumberToBinaryArray(uint(A_val), 16)
 	bitsB16 := ConvertNumberToBinaryArray(uint(B_val), 16)
-	fmt.Printf("\nWord A (28):    %v\n", bitsA16)
-	fmt.Printf("Word -A (-28):  %v\n", GetTwosComplement(bitsA16, 16))
-	fmt.Printf("Word B (2810):  %v\n", bitsB16)
-	fmt.Printf("Word -B (-2810):%v\n", GetTwosComplement(bitsB16, 16))
+	fmt.Printf("\nWord A (13):    %v\n", bitsA16)
+	fmt.Printf("Word -A (-13):  %v\n", GetTwosComplement(bitsA16, 16))
+	fmt.Printf("Word B (1306):  %v\n", bitsB16)
+	fmt.Printf("Word -B (-1306):%v\n", GetTwosComplement(bitsB16, 16))
 
 	// --- SHORTINT (32 bit) ---
 	bitsA32 := ConvertNumberToBinaryArray(uint(A_val), 32)
 	bitsB32 := ConvertNumberToBinaryArray(uint(B_val), 32)
 	bitsC32 := ConvertNumberToBinaryArray(uint(C_val), 32)
-	fmt.Printf("\nShortInt A (28):    %v\n", bitsA32)
-	fmt.Printf("ShortInt -A (-28):  %v\n", GetTwosComplement(bitsA32, 32))
-	fmt.Printf("ShortInt B (2810):  %v\n", bitsB32)
-	fmt.Printf("ShortInt -B (-2810):%v\n", GetTwosComplement(bitsB32, 32))
+	fmt.Printf("\nShortInt A (13):    %v\n", bitsA32)
+	fmt.Printf("ShortInt -A (-13):  %v\n", GetTwosComplement(bitsA32, 32))
+	fmt.Printf("ShortInt B (1306):  %v\n", bitsB32)
+	fmt.Printf("ShortInt -B (-1306):%v\n", GetTwosComplement(bitsB32, 32))
 	fmt.Printf("ShortInt C:         %v\n", bitsC32)
 	fmt.Printf("ShortInt -C:        %v\n", GetTwosComplement(bitsC32, 32))
 
@@ -44,10 +44,10 @@ func main() {
 	bitsA64 := ConvertNumberToBinaryArray(uint(A_val), 64)
 	bitsB64 := ConvertNumberToBinaryArray(uint(B_val), 64)
 	bitsC64 := ConvertNumberToBinaryArray(uint(C_val), 64)
-	fmt.Printf("\nLongInt A (28):    %v\n", bitsA64)
-	fmt.Printf("LongInt -A (-28):  %v\n", GetTwosComplement(bitsA64, 64))
-	fmt.Printf("LongInt B (2810):  %v\n", bitsB64)
-	fmt.Printf("LongInt -B (-2810):%v\n", GetTwosComplement(bitsB64, 64))
+	fmt.Printf("\nLongInt A (13):    %v\n", bitsA64)
+	fmt.Printf("LongInt -A (-13):  %v\n", GetTwosComplement(bitsA64, 64))
+	fmt.Printf("LongInt B (1306):  %v\n", bitsB64)
+	fmt.Printf("LongInt -B (-1306):%v\n", GetTwosComplement(bitsB64, 64))
 	fmt.Printf("LongInt C:         %v\n", bitsC64)
 	fmt.Printf("LongInt -C:        %v\n", GetTwosComplement(bitsC64, 64))
 
